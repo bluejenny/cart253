fe10d06f05b3108885a0dc58311fb22ebdd7c09a
jen <jlpoohachoff@gmail.com>
2020-09-11 22:38:38 -0400

    started drawing experiments

---
+++ experiments/drawing-experiments/js/script.js
@@ -0,0 +1,20 @@
+/**************************************************
+Template p5 project
+Pippin Barr
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+  background(127, 255, 20);
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}

f081bb77c1b7997d80fa3ba1e53d8980dd1ce567
jen <jlpoohachoff@gmail.com>
2020-09-12 10:50:04 -0400

    02-new alien

--- activities/02-draw-an-alien/js/script.js
+++ activities/02-draw-an-alien/js/script.js
@@ -11,30 +11,40 @@ Draw an alien on the canvas.
 function setup() {
   createCanvas(640, 480);
 
-  background(255, 100, 100);
+  background(186, 147, 202);
   noStroke();
 
   //body
-  fill(127);
-  ellipse(320, 480, 300, 200);
+  fill(142, 125, 99);
+  ellipse(320, 480, 175, 200);
 
   //head
-  fill(100);
-  ellipse(320, 240, 250, 400);
+  fill(181, 164, 138);
+  ellipse(320, 200, 300, 250);
+  ellipse(320, 230, 225, 200);
+  ellipse(320, 260, 150, 150);
+  ellipse(320, 290, 100, 100);
+  ellipse(320, 305, 50, 50);
+  ellipse(320, 315, 50, 60);
 
   //eyes
   fill(0);
-  ellipse(250, 240, 80, 250);
-  ellipse(390, 240, 80, 250);
+  //left
+  ellipse(260, 210, 85, 40);
+
+  //right
+  ellipse(380, 210, 85, 40);
+
   //nostrils
-  ellipse(300, 350, 10, 10);
-  ellipse(340, 350, 10, 10);
+  ellipse(315, 255, 5, 5);
+  ellipse(325, 255, 5, 5);
 
   //mouth
   stroke(200, 0, 0);
-  strokeWeight(5);
-  rectMode(CENTER);
-  rect(320, 390, 90, 25);
+  strokeWeight(3);
+  line(300, 295, 340, 295);
+  // rectMode(CENTER);
+  // rect(320, 290, 90, 25);
 
 }
 

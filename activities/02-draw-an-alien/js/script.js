c3f109209748f4bdba68b74532580db957557582
jen <jlpoohachoff@gmail.com>
2020-09-11 23:50:34 -0400

    experiments with shapes

--- activities/02-draw-an-alien/js/script.js
+++ activities/02-draw-an-alien/js/script.js
@@ -9,15 +9,6 @@ Here is a description of this template p5 project.
 //
 // Description of setup() goes here.
 function setup() {
-  createCanvas(500, 500);
-  background(50, 100, 255);
-  rectMode(CENTER);
-  rect(250, 250, 100, 100)
-  // point(250, 250);
-  // rect(0, 0, 250, 250);
-  // ellipse(250, 250, 100, 100);
-  // line(0, 0, 500, 500);
-  // line(500, 0, 0, 500);
 
 }
 

ead75a3cafe7653fbd3270e7f995aef54a770a04
jen <jlpoohachoff@gmail.com>
2020-09-13 16:13:37 -0400

    02 update alien and add background effect

--- activities/02-draw-an-alien/js/script.js
+++ activities/02-draw-an-alien/js/script.js
@@ -13,10 +13,29 @@ function setup() {
 
   background(186, 147, 202);
   noStroke();
+  angleMode(DEGREES)
+
+  //planet
+  fill(288, 210, 248);
+  ellipse(320, 350, 600, 550);
+  filter(BLUR, 80);
+
+  //shadow
+  fill(99, 44, 121);
+  ellipse(318, 500, 150, 200);
+  ellipse(318, 400, 25, 200);
+  ellipse(318, 200, 300, 250);
+  ellipse(318, 230, 225, 200);
+  ellipse(318, 260, 150, 150);
+  ellipse(318, 290, 100, 100);
+  ellipse(318, 305, 50, 50);
+  ellipse(318, 315, 50, 60);
+
 
   //body
-  fill(142, 125, 99);
-  ellipse(320, 480, 175, 200);
+  fill(163, 148, 124);
+  ellipse(320, 500, 150, 200);
+  ellipse(320, 400, 25, 200);
 
   //head
   fill(181, 164, 138);
@@ -27,22 +46,33 @@ function setup() {
   ellipse(320, 305, 50, 50);
   ellipse(320, 315, 50, 60);
 
+
+
   //eyes
-  fill(0);
   //left
-  ellipse(260, 210, 85, 40);
+  fill(0);
+  rotate(26);
+  ellipseMode(CORNER);
+  ellipse(280, 80, 95, 50);
+  rotate(-26);
 
   //right
-  ellipse(380, 210, 85, 40);
+  fill(0);
+  rotate(-26);
+  ellipse(207, 363, 95, 50);
+  rotate(26);
+  smooth();
 
+  //rotate(25);
   //nostrils
+  fill(0);
   ellipse(315, 255, 5, 5);
   ellipse(325, 255, 5, 5);
 
   //mouth
   stroke(200, 0, 0);
   strokeWeight(3);
-  line(300, 295, 340, 295);
+  line(310, 295, 335, 295);
   // rectMode(CENTER);
   // rect(320, 290, 90, 25);
 

77a6f1df7e04b6e0d400c08b409bd2445cdd9b7e
jen <jlpoohachoff@gmail.com>
2020-09-14 21:10:27 -0400

    added pixel alien

--- activities/02-draw-an-alien/js/script.js
+++ activities/02-draw-an-alien/js/script.js
@@ -10,41 +10,97 @@ Draw an alien on the canvas.
 // Draws an Alien
 function setup() {
   createCanvas(640, 480);
-
   background(186, 147, 202);
   noStroke();
   angleMode(DEGREES)
 
   //planet
-  fill(288, 210, 248);
-  ellipse(320, 300, 550, 550);
-  filter(BLUR, 20);
-
-  // fill(0, 164, 83);
-  // ellipse(90, 120, 125, 125);
-
-
-  //buildings
-  // fill(0);
-  // rect(30, 400, 55, 255);
-  // rect(100, 350, 85, 355);
+  squareColor = color(288, 210, 248);
+  squareColor.setAlpha(128 + 128 * sin(millis() / 1000));
+  fill(squareColor);
+  ellipse(320, 280, 525, 525);
 
   //shadow
   fill(99, 44, 121);
-  ellipse(318, 500, 150, 200);
-  ellipse(318, 400, 25, 200);
-  ellipse(318, 200, 300, 250);
-  ellipse(318, 230, 225, 200);
-  ellipse(318, 260, 150, 150);
-  ellipse(318, 290, 100, 100);
-  ellipse(318, 305, 50, 50);
-  ellipse(318, 315, 50, 60);
+  ellipse(317, 500, 150, 200);
+  ellipse(317, 400, 25, 200);
+  ellipse(317, 201, 300, 250);
+  ellipse(317, 231, 225, 200);
+  ellipse(317, 261, 150, 150);
+  ellipse(317, 291, 100, 100);
+  ellipse(317, 400, 30, 200);
+
+  filter(BLUR, 5);
+
+  //moon
+  // fill(240);
+  squareColor = color(288, 210, 248);
+  squareColor.setAlpha(128 + 128 * sin(millis() / 1000));
+  fill(squareColor);
+  ellipse(90, 120, 140, 140);
+
+  fill(80, 0, 255);
+  rectMode(CENTER);
+  rect(60, 90, 10, 10);
+  rect(120, 90, 10, 10);
+
+  rect(70, 100, 10, 10);
+  rect(110, 100, 10, 10);
+
+  rect(60, 110, 10, 10);
+  rect(70, 110, 10, 10);
+  rect(80, 110, 10, 10);
+  rect(90, 110, 10, 10);
+  rect(100, 110, 10, 10);
+  rect(110, 110, 10, 10);
+  rect(120, 110, 10, 10);
+
+  rect(50, 120, 10, 10);
+  rect(60, 120, 10, 10);
+  rect(80, 120, 10, 10);
+  rect(90, 120, 10, 10);
+  rect(100, 120, 10, 10);
+  rect(120, 120, 10, 10);
+  rect(130, 120, 10, 10);
+
+  rect(40, 130, 10, 10);
+  rect(50, 130, 10, 10);
+  rect(60, 130, 10, 10);
+  rect(70, 130, 10, 10);
+  rect(80, 130, 10, 10);
+  rect(90, 130, 10, 10);
+  rect(100, 130, 10, 10);
+  rect(110, 130, 10, 10);
+  rect(120, 130, 10, 10);
+  rect(130, 130, 10, 10);
+  rect(140, 130, 10, 10);
+
+  rect(40, 140, 10, 10);
+  rect(60, 140, 10, 10);
+  rect(70, 140, 10, 10);
+  rect(80, 140, 10, 10);
+  rect(90, 140, 10, 10);
+  rect(100, 140, 10, 10);
+  rect(110, 140, 10, 10);
+  rect(120, 140, 10, 10);
+  rect(140, 140, 10, 10);
+
+  rect(40, 150, 10, 10);
+  rect(60, 150, 10, 10);
+  rect(120, 150, 10, 10);
+  rect(140, 150, 10, 10);
+
+  rect(70, 160, 10, 10);
+  rect(110, 160, 10, 10);
+
+  rect(80, 160, 10, 10);
+  rect(100, 160, 10, 10);
 
 
   //body
   fill(163, 148, 124);
   ellipse(320, 500, 150, 200);
-  ellipse(320, 400, 25, 200);
+  ellipse(320, 400, 30, 200);
 
   //head
   fill(181, 164, 138);
@@ -52,8 +108,6 @@ function setup() {
   ellipse(320, 230, 225, 200);
   ellipse(320, 260, 150, 150);
   ellipse(320, 290, 100, 100);
-  ellipse(320, 305, 50, 50);
-  ellipse(320, 315, 50, 60);
 
   //eyes
   //left
@@ -69,15 +123,16 @@ function setup() {
   ellipse(207, 363, 95, 50);
   rotate(26);
 
-  // fill(255);
-  // ellipse(220, 210, 25, 25);
-  // ellipse(400, 210, 25, 25);
-  //
-  // fill(20);
-  // ellipse(220, 210, 22, 22);
-  // ellipse(400, 210, 22, 22);
+  //eyes white
+  fill(255);
+  ellipse(220, 210, 25, 25);
+  ellipse(400, 211, 25, 25);
 
-  //rotate(25);
+  //eyes pupil
+  fill(0);
+  ellipse(220, 212, 30, 30);
+  ellipse(396, 213, 30, 30);
+
   //nostrils
   fill(0);
   ellipse(315, 265, 5, 5);
@@ -87,8 +142,6 @@ function setup() {
   stroke(200, 0, 0);
   strokeWeight(3);
   line(310, 295, 335, 295);
-  // rectMode(CENTER);
-  // rect(320, 290, 90, 25);
 
 }
 

01d5805516ee0aa4e64cc08a1ced2aaa914e94ac
jen <jlpoohachoff@gmail.com>
2020-09-20 18:53:06 -0400

    03-Starting Activity 3

--- activities/03-moving-pictures/js/script.js
+++ activities/03-moving-pictures/js/script.js
@@ -8,25 +8,13 @@ Here is a description of this template p5 project.
 // setup()
 //
 // Description of setup() goes here.
-// function setup() {
-//   createCanvas(500, 500);
-//
-// }
-
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
-  background(mouseX, mouseY, mouseY / 2);
-  rectMode(CENTER);
-  rect(mouseX, mouseY, mouseX, mouseY);
 
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
 }

57a8017889694cedc08ecec672b02f4351e28d0d
jen <jlpoohachoff@gmail.com>
2020-09-21 21:03:07 -0400

    activity 3 - drawing circles completed

--- activities/03-moving-pictures/js/script.js
+++ activities/03-moving-pictures/js/script.js
@@ -1,14 +1,40 @@
 /**************************************************
-Template p5 project
-Pippin Barr
-
-Here is a description of this template p5 project.
+03 Moving Parts
+Jen Poohachoff
 **************************************************/
+
+let bg = {
+  r: 0,
+  g: 0,
+  b: 0
+}
+
+let circle1 = {
+  x: 0,
+  y: 250,
+  size: 100,
+  growth: 1,
+  speed: 1,
+  fill: 255,
+  alpha: 200
+}
+
+let circle2 = {
+  x: 500,
+  y: 250,
+  size: 75,
+  sizeRatio: .75,
+  speed: -1,
+  fill: 255,
+  alpha: 200
+}
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
+  createCanvas(500, 500);
+  noStroke();
 
 }
 
@@ -16,5 +42,26 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
+  //background animation
+  background(bg.r, bg.g, bg.b);
+  bg.r = map(circle1.size, 100, width, 0, 255);
+
+  //circle animation
+
+  //left
+  circle1.x += circle1.speed;
+  circle1.x = constrain(circle1.x, 0, width/2);
+  circle1.size += circle1.growth;
+  circle1.size = constrain(circle1.size, 0, width);
+  fill(circle1.fill, circle1.alpha);
+  ellipse(circle1.x, circle1.y, circle1.size);
+
+  //right
+  circle2.x += circle2.speed;
+  circle2.x = constrain(circle2.x, width/2, width)
+  circle2.size = circle1.size * circle2.sizeRatio;
+  fill(circle2.fill, circle2.alpha);
+  ellipse(circle2.x, circle2.y, circle2.size);
+
 
 }

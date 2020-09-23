f5af906e3d1d0c4bff125f575c8eae7e0ff03b47
jen <jlpoohachoff@gmail.com>
2020-09-22 22:24:50 -0400

    Exercise 1: update animation

--- exercises/exercise1/js/script.js
+++ exercises/exercise1/js/script.js
@@ -1,5 +1,5 @@
 /**************************************************
-03 Moving Parts
+03 I like to move it!
 Jen Poohachoff
 **************************************************/
 
@@ -9,47 +9,32 @@ let bg = {
   b: 0
 }
 
-let circle1 = {
-  x: 0,
-  y: 250,
-  size: 100,
-  growth: 1,
-  speed: 1,
-  fill: 255,
-  alpha: 200
-}
-
-let circle2 = {
-  x: 500,
-  y: 250,
-  size: 75,
-  sizeRatio: .25,
-  speed: -1,
-  fill: 255,
-  alpha: 200
-}
-
-
 let rect1 = {
-  x: 30,
-  y: 20,
-  width: 10,
-  height: 55,
-  // growth: 1,
-  // speed: 1,
-  // fill: 255,
-  // alpha: 200
+  x: 0,
+  y: 70,
+  width: 100,
+  height: 150,
+  speed: 5
+}
+
+let rect2 = {
+  x: 1000,
+  y: 220,
+  width: 100,
+  height: 150,
+  speed: -2
 }
 
 let circ1 = {
   x: 160,
-  y: 99,
-  size: 50,
+  y: 0,
+  fill: 0,
+  size: 25,
+  speed: 1.25,
+  growth: .15
 }
 
 // setup()
-//
-// Description of setup() goes here.
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
@@ -57,45 +42,36 @@ function setup() {
 }
 
 // draw()
-//
-// Description of draw() goes here.
 function draw() {
-  //background animation
+
+  //background color maps green and blue values to
+  //mouseX and mouseY, r gets larger as circle
+  //moves down screen
   background(bg.r, bg.g, bg.b);
+  bg.r = map(circ1.y, 0, height, 0, 255);
   bg.g = map(mouseY, 0, width, 0, 255);
   bg.b = map(mouseX, 0, height, 0, 255);
-  // bg.b = map(mouseY, 0, width, 0, 255);
-  //bg.b = (mouseX * mouseY) / 2;
-  // bg.b = mouseX + mouseY;
+  // bg.r = random(0, 255);
 
-  //draw dominoes
+  //position of circle always in middle
+  circ1.x = width/2;
+
+  //animation of rectangles
+  fill(255, 200);
   rect(rect1.x, rect1.y, rect1.width, rect1.height);
-  rect(rect1.x +20, rect1.y, rect1.width, rect1.height);
-  rect(rect1.x +40, rect1.y, rect1.width, rect1.height);
-  rect(rect1.x +60, rect1.y, rect1.width, rect1.height);
-  rect(rect1.x +80, rect1.y, rect1.width, rect1.height);
+  rect(rect2.x, rect2.y, rect2.width, rect2.height);
+  rect1.x += rect1.speed;
+  rect1.x = constrain(rect1.x, 0, width/2-50);
+  rect2.x += rect2.speed;
+  rect2.x = constrain(rect2.x, width/2-50, width );
 
+  //animation of circle
+  fill(255);
   ellipse(circ1.x, circ1.y, circ1.size);
-
-  //rect(130, 100, 60, 75, 50, 50, 5, 5);
-
-  //circle animation
-
-  //left
-  circle1.x += circle1.speed;
-  circle1.x = constrain(circle1.x, 0, width);
-  circle1.size += circle1.growth;
-  circle1.size = constrain(circle1.size, 0, width);
-  fill(circle1.fill, circle1.alpha);
-  ellipse(circle1.x, circle1.y, circle1.size);
-
-  //right
-  circle2.x += circle2.speed;
-  circle2.x = constrain(circle2.x, 0, width)
-  circle2.size = circle1.size * circle2.sizeRatio;
-  fill(circle2.fill, circle2.alpha);
-  ellipse(circle2.x, circle2.y, circle2.size);
-
+  circ1.y += circ1.speed;
+  circ1.y = constrain(circ1.y, 0, rect2.y + rect2.height + 100 );
+  circ1.size += circ1.growth;
+  circ1.size = constrain(circ1.size, 0, 125 );
 
 }
 

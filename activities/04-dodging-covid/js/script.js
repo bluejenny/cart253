38171c62db9014b95521acefe61f04919681520d
jen <jlpoohachoff@gmail.com>
2020-09-28 22:08:14 -0400

    A4 - Started Dodging Covid assignment

---
+++ activities/04-dodging-covid/js/script.js
@@ -0,0 +1,27 @@
+/**************************************************
+Activity 4 : Dodging Covid
+Jennifer Poohachoff
+
+Class Activity
+**************************************************/
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+}
+
+
+function draw() {
+  background(127);
+
+  push();
+
+  pop();
+
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
--- experiments/04-7-intermediate-drawings/js/script.js
+++ experiments/04-7-intermediate-drawings/js/script.js
@@ -5,11 +5,14 @@ Pippin Barr
 Here is a description of this template p5 project.
 **************************************************/
 
+let angle = 0;
+let rectScale = 0;
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
   createCanvas(windowWidth, windowHeight);
+    // createCanvas(500, 500);
 
 }
 
@@ -17,5 +20,31 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
+  background(127);
 
+  // push();
+  // fill(255, 0, 0);
+  // stroke(0, 255, 255);
+  // strokeWeight(10);
+  // rect(100, 100, 100, 100);
+  // pop();
+  //
+  // push();
+  // fill(0, 0, 255);
+  // rect(300, 100, 100, 100);
+  // pop();
+
+  push();
+  fill(255, 0, 0);
+  rectMode(CENTER);
+  // rotate(radians(45));
+  //rotate(PI/4);
+  translate(width/2, height/2);
+  rotate(angle);
+  scale(rectScale);
+  rect(0, 0, 100, 100);
+  pop();
+
+  angle = angle + .01;
+  rectScale = rectScale + .01;
 }

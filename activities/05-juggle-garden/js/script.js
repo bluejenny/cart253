8a38846d205485b483ec6beb419f7662945d9ea8
jen <jlpoohachoff@gmail.com>
2020-10-26 22:44:46 -0400

    finished juggle activity

--- activities/05-juggle-garden/js/script.js
+++ activities/05-juggle-garden/js/script.js
@@ -1,22 +1,42 @@
 "use strict";
 
 /**************************************************
-Template p5 project
-Pippin Barr
-
-Here is a description of this template p5 project.
+05 Juggle Garden
+with Pippin Barr
 **************************************************/
 
-// setup()
-//
-// Description of setup() goes here.
-function setup() {
+let gravityForce = 0.0025;
+let paddle;
+let balls = [];
+let numBalls = 13;
 
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  paddle = new Paddle(300, 20);
+
+  for (let i = 0; i < numBalls; i++) {
+    let x = random(0, width);
+    let y = random(-400, -100);
+    let ball = new Ball(x, y);
+    balls.push(ball);
+  }
 }
 
-// draw()
-//
-// Description of draw() goes here.
-function draw() {
 
+function draw() {
+  background(0);
+
+  paddle.move();
+  paddle.display();
+
+  for (let i = 0; i < balls.length; i++) {
+    let ball = balls[i];
+    if (ball.active) {
+    ball.gravity(gravityForce);
+    ball.move();
+    ball.bounce(paddle);
+    ball.display();
+    }
+  }
 }

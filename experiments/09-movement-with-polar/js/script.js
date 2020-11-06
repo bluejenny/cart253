5e6d68b310715d37ad4bded09add8b8c563f484c
jen <jlpoohachoff@gmail.com>
2020-11-05 22:54:10 -0500

    experiement with polar coordinates

---
+++ experiments/09-movement-with-polar/js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/**************************************************
+09 - Movement with Polar Coordinates
+**************************************************/
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+}
+
+function draw() {
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}

2131519043bfe2e009a09a16c9f3f3200fc18373
jen <jlpoohachoff@gmail.com>
2020-11-07 10:58:16 -0500

    started exercises 10 more with sound

---
+++ experiments/10-reintroducingsound/js/script.js
@@ -0,0 +1,40 @@
+"use strict";
+
+/**************************************************
+06 - Sound
+with Pippin Barr
+**************************************************/
+
+let barkSFX;
+let sighSFX;
+
+
+function preload() {
+  barkSFX = loadSound(`assets/sounds/bark.wav`);
+  sighSFX = loadSound(`assets/sounds/sigh.wav`);
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  userStartAudio();
+}
+
+
+function draw() {
+  background(0);
+
+  let newRate = map(mouseX, 0, width, -3, 3);
+  barkSFX.rate(newRate);
+}
+
+function mousePressed() {
+  barkSFX.loop();
+}
+
+function keyPressed() {
+  sighSFX.play();
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}

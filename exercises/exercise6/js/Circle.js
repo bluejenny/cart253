4d9f73a33be56b4474c8eb40e4de65257d455dbf
jen <jlpoohachoff@gmail.com>
2020-11-10 22:49:11 -0500

    Exercise 6 _ working on circle / sound combination

---
+++ exercises/exercise6/js/Circle.js
@@ -0,0 +1,52 @@
+class Circle {
+  constructor(x, y, note) {
+    this.x = x;
+    this.y = y;
+    this.size = 50;
+    this.stroke = {
+      r: random(0, 255),
+      g: random(0, 255),
+      b: random(0, 255),
+    };
+    this.strokeWeight = 1;
+    this.strokeWeightGrowth = 0.025;
+    this.growSize = 10;
+    this.colorChange = 5;
+    this.growing = true;
+
+    //Synth
+    this.note = note;
+    this.synth = new p5.PolySynth();
+  }
+
+  grow() {
+    if (this.growing) {
+      this.size += this.growSize;
+      this.strokeWeight += this.strokeWeightGrowth;
+      this.stroke.r += this.colorChange;
+      this.stroke.g += this.colorChange;
+      this.stroke.b += this.colorChange;
+    }
+
+    if (this.y - this.size / 2 < 0 || this.y + this.size / 2 > height) {
+      stop();
+      this.strokeWeight = 0;
+      this.growing = false;
+    } else {
+      this.playNote();
+    }
+  }
+
+  playNote() {
+    this.synth.play(this.note, 0.01, 0, 0.1);
+  }
+
+  display() {
+    push();
+    strokeWeight(this.strokeWeight);
+    noFill();
+    stroke(this.stroke.r, this.stroke.g, this.stroke.b, 200);
+    ellipse(this.x, this.y, this.size);
+    pop();
+  }
+}
--- exercises/exercise6/js/script.js
+++ exercises/exercise6/js/script.js
@@ -5,45 +5,142 @@ Exercise 6: Make Some Noise
 Jen Poohachoff
 **************************************************/
 
-// The Balls
-let balls = [];
+//darker background behind animation to make it appear as if getting darker
+let bgDark = {
+  r: 48,
+  g: 65,
+  b: 79,
+};
+
+//sky bacground, opacity fades as mouseY moves down
+let bgLight = {
+  r: 202,
+  g: 225,
+  b: 244,
+};
+
+// the circles
+let circles = [];
 
 // F-minor
 let notes = [`F3`, `G3`, `Ab4`, `Bb4`, `C4`, `Db4`, `Eb4`, `F4`];
 
-
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  userStartAudio();
+  // userStartAudio();
 }
 
-
 function draw() {
-  background(0);
+  background(bgDark.r, bgDark.g, bgDark.b);
 
-  for (let i = 0; i < balls.length; i++) {
-    let ball = balls[i];
-    ball.move();
-    ball.bounce();
-    ball.display();
+  // 2nd background that decreases opacity
+  // as mouseY moves towards height
+  backgroundFade();
+
+  // show + where user can click
+  displayMarkers();
+
+  for (let i = 0; i < circles.length; i++) {
+    let circle = circles[i];
+    circle.grow();
+    circle.display();
   }
 }
 
+// creates effect of canvas getting lighter or darker
+// based on mouseY position, 0 = light, height = dark
+
+function backgroundFade() {
+  let m = map(mouseY, 0, height, 235, 0);
+  fill(bgLight.r, bgLight.g, bgLight.b, m);
+  rect(0, 0, width, height); //light sky
+}
+
+function displayMarkers() {
+  //displays a graph of + markers to show user where to click
+  push();
+  let m = map(mouseY, 0, height, 235, 0);
+  strokeWeight(5);
+  stroke(47, m);
+
+  // example of points where sound will play --> top line
+  line(width / 2 - 5, height / 2 - 200, width / 2 + 5, height / 2 - 200);
+  line(width / 2, height / 2 - 205, width / 2, height / 2 - 195);
+
+  line(width / 6 - 5, height / 2 - 200, width / 6 + 5, height / 2 - 200);
+  line(width / 6, height / 2 - 205, width / 6, height / 2 - 195);
+
+  line(
+    (width / 6) * 5 - 5,
+    height / 2 - 200,
+    (width / 6) * 5 + 5,
+    height / 2 - 200
+  );
+  line((width / 6) * 5, height / 2 - 205, (width / 6) * 5, height / 2 - 195);
+
+  // example of points where sound will play --> center line
+  line(width / 2 - 5, height / 2, width / 2 + 5, height / 2);
+  line(width / 2, height / 2 - 5, width / 2, height / 2 + 5);
+
+  line(width / 6 - 5, height / 2, width / 6 + 5, height / 2);
+  line(width / 6, height / 2 - 5, width / 6, height / 2 + 5);
+
+  line((width / 6) * 5 - 5, height / 2, (width / 6) * 5 + 5, height / 2);
+  line((width / 6) * 5, height / 2 - 5, (width / 6) * 5, height / 2 + 5);
+
+  // example of points where sound will play --> bottom line
+  line(width / 2 - 5, height / 2 + 200, width / 2 + 5, height / 2 + 200);
+  line(width / 2, height / 2 + 205, width / 2, height / 2 + 195);
+
+  line(width / 6 - 5, height / 2 + 200, width / 6 + 5, height / 2 + 200);
+  line(width / 6, height / 2 + 205, width / 6, height / 2 + 195);
+
+  line(
+    (width / 6) * 5 - 5,
+    height / 2 + 200,
+    (width / 6) * 5 + 5,
+    height / 2 + 200
+  );
+  line((width / 6) * 5, height / 2 + 205, (width / 6) * 5, height / 2 + 195);
+  pop();
+}
 function mousePressed() {
-  createBall(mouseX, mouseY);
+  if (
+    mouseX >= width / 2 - 5 &&
+    mouseX <= width / 2 + 5 &&
+    mouseY > height / 2 - 5 &&
+    mouseY < height / 2 + 5
+  ) {
+    createCircle(mouseX, mouseY);
+  }
+
+  if (
+    mouseX >= width / 6 - 5 &&
+    mouseX <= width / 6 + 5 &&
+    mouseY > height / 2 - 5 &&
+    mouseY < height / 2 + 5
+  ) {
+    createCircle(mouseX, mouseY);
+  }
+
+  if (
+    mouseX >= (width / 6) * 5 - 5 &&
+    mouseX <= (width / 6) * 5 + 5 &&
+    mouseY > height / 2 - 5 &&
+    mouseY < height / 2 + 5
+  ) {
+    createCircle(mouseX, mouseY);
+  }
 }
 
-function createBall(x, y) {
+function createCircle(x, y) {
   let note = random(notes);
-  let ball = new Ball(x, y, note);
-  balls.push(ball);
+  let circle = new Circle(x, y, note);
+  circles.push(circle);
 }
 
-function keyPressed() {
-}
-
+function keyPressed() {}
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
